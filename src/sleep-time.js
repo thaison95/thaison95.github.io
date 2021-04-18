dbc58a557bba93a4312bcf6b8cc6828d2d360ad9
thaison <thaisonnguyen8295@gmail.com>
2021-04-18 23:26:20 +0700

    update msg

--- src/sleep-time.js
+++ src/sleep-time.js
@@ -8,38 +8,77 @@ if (time.hour() === 23 || time.hour() < 4) {
 
 window.addEventListener("load", () => {
   if (!isSleepTime) return;
-  writeDB('loaded', { msg: 'sleep' });
+  writeDB("loaded", { msg: "sleep" });
 
-  document.getElementById('container').style.display = "none";
-  document.getElementById('sleep').style.display = "block";
-  document.getElementById('msg').style.marginTop = "0";
+  document.getElementById("container").style.display = "none";
+  document.getElementById("sleep").style.display = "block";
+  document.getElementById("msg").style.marginTop = "0";
 });
 
 function onSleepClick() {
-  writeDB('click-sleep-' + sleepClickCount, { count: sleepClickCount });
+  writeDB("click-sleep-" + sleepClickCount, { count: sleepClickCount });
+  if (time.hour() >= 0 && time.hour() < 4) {
+    switch (sleepClickCount) {
+      case 0:
+        showMsg("Nếu em đọc được dòng này thì");
+        break;
+      case 1:
+        showMsg("Anh chỉ muốn nói là");
+        break;
+      case 2:
+        showMsg("Anh");
+        break;
+      case 3:
+        showMsg("muốn");
+        break;
+      case 4:
+        showMsg("gặp");
+        break;
+      case 5:
+        showMsg("em");
+        break;
+      case 6:
+        showMsg("ở");
+        break;
+      case 7:
+        showMsg("kon tum");
+        break;
+      case 8:
+        showMsg("thôi");
+        break;
+      case 9:
+        showMsg("Ngủ đi.");
+        document.getElementById("sleepImg").style.display = "none";
+        document.getElementById("typingImg").style.display = "inline";
+        break;
+    }
+    sleepClickCount++;
+    return;
+  }
   switch (sleepClickCount) {
     case 0:
-      showMsg('Ngủ đi. Anh lock app từ 11h tới 4h rồi.');
+      showMsg("Ngủ đi. Anh lock app từ 11h tới 4h rồi.");
       break;
     case 1:
-      showMsg('Đừng nhấn nữa.');
+      showMsg("Đừng nhấn nữa.");
       break;
     case 2:
-      showMsg('Đã bảo đừng nhấn nữa gòi.');
+      showMsg("Đã bảo đừng nhấn nữa gòi.");
       break;
     case 3:
-      showMsg('Lì ghê.');
+      showMsg("Lì ghê.");
       break;
     case 4:
-      showMsg('');
-      document.getElementById('sleepImg').style.display = "none";
-      document.getElementById('typingImg').style.display = "inline";
+      showMsg("");
+      document.getElementById("sleepImg").style.display = "none";
+      document.getElementById("typingImg").style.display = "inline";
       break;
   }
   sleepClickCount++;
 }
 
 function onTypingClick() {
-  document.getElementById('sleepImg').style.display = "inline";
-  document.getElementById('typingImg').style.display = "none";
+  showMsg("");
+  document.getElementById("sleepImg").style.display = "inline";
+  document.getElementById("typingImg").style.display = "none";
 }
